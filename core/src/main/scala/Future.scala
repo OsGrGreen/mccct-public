b45f62dfe3b7b5f11a18a51de9f3f8b22c149e58
ogronman <90456429+OsGrGreen@users.noreply.github.com>
2025-06-26 17:32:21 +0200

    Add ability to wait for maximum number of tasks
    The scheduler now waits until it knows that it must
    make a choice, since it is impossible to continue otherwise.
    This is done by adding one `AtomicBoolean` and one condition.
    The `AtomicBoolean` is necessary to signal if we should execute
    the scheduler as normal. This can happen in two cases, either we
    have gotten to the end (`awaitTermination`) or we call `await`
    on a top level task. The reason why it is not enough to simply
    send a signal is that that would only allow a singular choice.
    However, there is no guarantee that we would make the "correct"
    choice of `awaited` task.
    
    Fix bug with main thread stalling after termination
    
    There was a bug in which if we `awaited` a top level
    task, but had other tasks ready then we may call `termination`
    before the main thread has properly started. This can
    result in the scheduler thread being signaled and tricking
    it into terminating. While the main thread is still waiting.
    This could be solved by making sure that `cnt` will never be
    zero while we wait for a `top level` task.
    
    Add comments to explain changes and functionality
    
    Add tests for waiting until queue is filled
    
    To increase the confidence in the logic for making
    the scheduler wait as long as possible to make a
    choice, more unit tests have been added.

--- core/src/main/scala/Future.scala
+++ core/src/main/scala/Future.scala
@@ -50,12 +50,19 @@ abstract class Task(val parent: Task, init: Boolean = true, isEnd: Boolean = fal
   var id:Id = Id(parent, isEnd)
   final def isReady(): Boolean = ready.get()
   def run(): Unit
+  final def isTop: Boolean = parent == rootTask
 
-  override def toString(): String = s"[${id.getId()}]"
+  override def toString(): String = s"[${id.getId()},${id.getNumChildren()} , ${isRoot}, ${isTop}]"
 }
 
 class Future[T](underlying: async.Future[T]) {
   def await(using ac: async.Async, task: Task): T = {
+    /** Signal the scheduler that we are waiting for something
+     *  If the task calling await is top-level, then `task` will be the root task
+     *  Waiting for a top-level task means that the scheduler is in a "stuck" state
+     *  and must execute the awaited task before it is able to continue
+     */
+    Scheduler.stuckSignal(task)
     val resultOrFailure = underlying.awaitResult
     // surrounding task is now ready
     task.ready.set(true)
@@ -67,6 +74,14 @@ class Future[T](underlying: async.Future[T]) {
 
     // wait for scheduler to resume task
     taskController.awaitResult
+    /** Signal the scheduler that it is no longer in a stuck state
+     *  If a top level task was awaited then the Scheduler should now suspend execution
+     *  until all top-level tasks have been submitted to the scheduler or
+     *  the scheduler reaches another stuck state
+     * 
+     *  Also allows the scheduler to terminate 
+     */  
+    Scheduler.noLongerStuck(task)
 
     resultOrFailure.get
   }
@@ -80,14 +95,20 @@ object Scheduler {
   private val lock: Lock = new ReentrantLock
   private val queueChange = lock.newCondition()
   private val termination = lock.newCondition()
+  /** Used to signal when execution from a stuck state must continue   */
+  private val stuckState = lock.newCondition()
 
   private var readyTasks: List[(Task, Controller)] = List()
   private var targetSchedule: List[String] = List()
 
   private var schedule: List[String] = List()
 
+  /** Determines if scheduler is allowed to continue execution */
+  private var hasAllTasks: Boolean = false
+
   def start(initTargetSchedule: List[String] = List()) =
     Scheduler.reset()
+
     targetSchedule = initTargetSchedule
     val shouldFollow = !targetSchedule.isEmpty
     val schedulerTask = new Runnable {
@@ -95,17 +116,22 @@ object Scheduler {
         while (!done) {
           lock.lock()
           try
-            if (readyTasks.size == 0){
+            if !hasAllTasks then
+              println("scheduler waiting to get unstuck")
+              stuckState.awaitUninterruptibly()
+            
+            if (readyTasks.size == 0 && !done){
               println("scheduler waiting for change in task queue...")
               queueChange.awaitUninterruptibly()
+              println("scheduler got change in task queue...")
             }
-            if cnt == 0 && readyTasks.size == 0 then
+            
+            if hasFinished then
               done = true
               termination.signal()
               return 
                 
             println(s"scheduler: size of task queue = ${readyTasks.size}")
-
 
             val (task, ctrl) = if shouldFollow && !targetSchedule.isEmpty then
               followSchedule()
@@ -144,11 +170,50 @@ object Scheduler {
   def awaitTermination() =
     lock.lock()
     try
+      //The end of the main thread has been reached
+      hasAllTasks = true //All top level tasks must now be available for the scheduler
+      stuckState.signal()
+      if hasFinished then queueChange.signal()
       if !done then termination.awaitUninterruptibly()
     finally
       schedule = schedule.reverse
       lock.unlock()
 
+  /** Signals the scheduler to execute if in stuck state
+   *  
+   *  If task is a root task, then the awaited task must be a top level task
+   *  In this case signal the scheduler to execute until other instructions are given
+   * @param task the task that has been suspended
+   */
+  private[mccct] def stuckSignal(task: Task) = 
+    lock.lock()
+    try
+      //If parent is null, then this must be a root task
+      if task.isRoot then //Which means that execution must continue until the awaited top-level task is completed
+        hasAllTasks = true //Allow scheduler to execute until `hasAllTasks` is set to false
+        cnt += 1 //Make sure that the scheduler can not finish while waiting for a top-level task
+        stuckState.signal() //Signal the lock that the scheduler must continue
+    finally
+      lock.unlock()
+
+  /** Signals the scheduler to wait until stuck or all top-level tasks have been loaded
+   *  
+   *  If task is a root task, then the awaited task must be a top level task
+   *  In this case signal the scheduler to wait until notified otherwise
+   * @param task the task that has been suspended
+   */
+  private[mccct] def noLongerStuck(task: Task) = 
+    lock.lock()
+    try
+      //If parent is null, then this must be a root task
+      if task.isRoot then //Which means that execution must continue until the awaited top-level task is completed
+        hasAllTasks = false //In this case we have now executed the blocking task, and may once again wait until we have loaded all top-level tasks (or become stuck)
+        cnt -= 1 //Scheduler may now finish when possible
+    finally
+      lock.unlock()
+
+  def hasFinished: Boolean = cnt == 0 && readyTasks.size == 0 && hasAllTasks
+  
   private[mccct] def submit(task: Task, taskController: async.Future.Promise[Boolean], shouldIncrement: Boolean = true): Unit =
     lock.lock()
     try
@@ -165,7 +230,7 @@ object Scheduler {
     lock.lock()
     try
       cnt -= 1
-      if cnt == 0 && readyTasks.size == 0 then
+      if hasFinished then
         queueChange.signal()
     finally
       lock.unlock()
@@ -177,6 +242,7 @@ object Scheduler {
     rootTask.id.reset()
     schedule = List()
     targetSchedule = List()
+    hasAllTasks = false
 
   def getDone(): Boolean = done
 }
